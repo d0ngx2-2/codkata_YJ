b61ebb020f8752acfd9bda1c480efb01c9ac5c85
d0ngx2-2 <abc157898@gmail.com>
2025-11-06 09:33:57 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.12 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long solution(long num) {
+        if(num == 1)return 0;
+        for(int i = 0; i < 500; i++) {
+            if(num == 1) {
+                return i;
+            }
+            else if(num % 2 == 0) {
+                num = num / 2;
+            } else {
+                num = num * 3 + 1;
+            }
+        }    
+        return -1;
+    }
+}
