aa8b770c4e42c02381775c91fa0f62bd7ec18d53
d0ngx2-2 <abc157898@gmail.com>
2025-12-06 21:52:33 +0900

    [Silver V] Title: 거스름돈, Time: 168 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.java
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -5,24 +5,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        int answer = -1;
-
-        // 5원을 최대한 많이 쓴다.
-        int five = n / 5;
-
-        while (five >= 0) {
-            int remain = n - (five * 5);
-
-            // 나머지가 0이면 끝
-            if (remain % 2 == 0) {
-                answer = five + (remain / 2);
-                break;
-            }
-
-            // 2로 안 나누어지면 5원 하나 줄여서 다시
-            five--;
+        // 예외 처리
+        if (n == 1 || n == 3) {
+            System.out.println(-1);
+            return;
         }
 
-        System.out.println(answer);
+        int five = n / 5;
+
+        // 나머지 1 또는 3 → 5원 하나 빼서 조정
+        if (n % 5 == 1 || n % 5 == 3) {
+            five -= 1;
+        }
+
+        int remain = n - (five * 5);
+        int two = remain / 2;
+
+        System.out.println(five + two);
     }
 }
